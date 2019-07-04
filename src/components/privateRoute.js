34426a248348b22677487b29d459d23c0aa06ffa
siemah <m_saidouni@inptic.edu.dz>
2019-07-04 17:45:55 +0100

    (Build)Private route Component

---
+++ src/components/privateRoute.js
@@ -0,0 +1,16 @@
+import React, { useEffect } from 'react'
+import { navigate } from 'gatsby'
+import { isLoggedIn, } from '../services/auth'
+
+export default function PrivateRoute({ component: Component, location, ...rest}) {
+ 
+  useEffect(() => {
+    let noOnLoginPage = location.pathname !== `/app/login`
+    if (!isLoggedIn() && noOnLoginPage) {
+      navigate("/app/login");
+      //return null
+    }
+  }, []);
+
+  return <Component {...rest} />
+}
