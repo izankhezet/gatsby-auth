37d7f7bd117398fc8d5251e0d337ece1d3411baf
siemah <m_saidouni@inptic.edu.dz>
2019-07-04 19:26:25 +0100

    (Build)add a backend route to auth requests

---
+++ api/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true, }));
+
+app.get('/', (req, res) => {
+    const  { body } = req, errors = [];
+    console.log(body);
+    
+    if( 
+        ( !('username' in body) && body.username !== 'john' ) ||
+        ( !('password' in body) && body.password !== 'pass' )
+    ) {
+        return res.json({
+            status: 'NO',
+            error: [
+                'Pair username/password failed',
+            ]
+        })
+    }
+    res.json({
+        status: 'OK',
+        user: {
+            username: 'siemah',
+            token: 'user-token',
+        },
+    })
+});
+
+app.listen(3000, err => {
+    console.log(`server runing on PORT: 3000`);
+})
