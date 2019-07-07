0a418eb4f18ac58c312e44fb2b97021a4dd1c310
siemah <m_saidouni@inptic.edu.dz>
2019-07-07 19:56:29 +0200

    (Build)Add gatsby lifecycle wrapeRootElement Move a Splash from wrapePageElement to wrapeRootElement

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,8 +5,30 @@
  * @see: https://www.gatsbyjs.org/docs/browser-apis/
  */
 const React = require('react');
-const { Splash } = require('./src/components/Splash')
+const { Splash } = require('./src/components/Splash');
+const { Provider, authState } = require('./src/store/context/auth');
+const { useState, } = React;
 
-exports.wrapPageElement = ({ element, props }) => {
-    return <Splash {...props}>{element}</Splash>
-  }
+const AuthProvider = ({ children }) => {
+  const [auth, setAuth] = useState(authState);
+
+  return (
+    <Provider value={{
+      ...auth,
+      setAuth,
+    }}>
+      <Splash>{ children }</Splash>
+    </Provider>
+  )
+}
+
+// wrape a whole page by a Splash screen
+/*exports.wrapPageElement = ({ element, props }) => {
+  return <Splash {...props}>{element}</Splash>
+}*/
+// wrap a whole app by a provider to use global state
+// in this case we use React context api rather than redux/mobx ..
+exports.wrapRootElement = ({element}) => {
+  // const [auth, setAuth] = useState(authState);
+  return <AuthProvider>{ element }</AuthProvider>
+}
