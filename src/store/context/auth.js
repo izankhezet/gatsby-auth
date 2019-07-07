01e8e574863647b134c600a14ae93deee177c529
siemah <m_saidouni@inptic.edu.dz>
2019-07-07 19:55:26 +0200

    (Build)Create auth context to determine the state of auth of user

---
+++ src/store/context/auth.js
@@ -0,0 +1,10 @@
+import React, { createContext } from 'react';
+// auth context default value
+export const authState = {
+  username: null,
+  email: null,
+};
+// auth context
+const AuthContext = createContext(authState);
+// Provider component
+export const { Provider } = AuthContext;
