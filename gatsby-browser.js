700c6f14177b7676b6c996ad87ed2587388baa5f
siemah <m_saidouni@inptic.edu.dz>
2019-07-07 19:31:12 +0200

    (Build)move a Splash component to separate file

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,49 +4,8 @@
  * @version 1.0.0
  * @see: https://www.gatsbyjs.org/docs/browser-apis/
  */
-
 const React = require('react');
-const Axios = require('axios');
-const { checkingAuth, setUser } = require('./src/services/auth')
-const { useState, useEffect } = React;
-
-const Splash = ({children}) =>  {
-  const [loaded, setLoaded] = useState(false);
-  useEffect(() => {
-    const fetch = async () => {
-      console.log('checking ..');
-      let { data } = await checkingAuth();
-      if( data && data.status === 'OK' ) {
-          setUser(data.user);
-      }
-      setLoaded(true);
-    }
-    fetch()
-  }, []);
-
-  if(loaded) {
-    return (
-      <>{ children }</>
-    )
-  }
-  return (
-        <div style={{
-            position:'fixed',
-            top: 0,
-            left: 0,
-            height: '100vh',
-            width: '100vw',
-            backgroundColor: '#333',
-            zIndex: 1000,
-            color: '#666',
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center'
-        }}>
-            <h1>Splash Screen</h1>
-        </div>
-    )
-}
+const { Splash } = require('./src/components/Splash')
 
 exports.wrapPageElement = ({ element, props }) => {
     return <Splash {...props}>{element}</Splash>
--- src/components/Splash.js
+++ src/components/Splash.js
@@ -1,19 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect, } from 'react';
+import { checkingAuth, setUser, } from '../services/auth';
 
-const Splash = (props) => {
-    return (
+export const Splash = ({children}) =>  {
+  const [loaded, setLoaded] = useState(false);
+  useEffect(() => {
+    const fetch = async () => {
+      let { data } = await checkingAuth();
+      if( data && data.status === 'OK' ) {
+          setUser(data.user);
+      }
+      setLoaded(true);
+    }
+    fetch()
+  }, []);
+
+  if(loaded) return <>{ children }</>
+  return (
         <div style={{
             position:'fixed',
             top: 0,
             left: 0,
             height: '100vh',
             width: '100vw',
-            backgroundColor: 'purpple',
+            backgroundColor: '#333',
             zIndex: 1000,
-            color: 'white'
+            color: '#666',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center'
         }}>
             <h1>Splash Screen</h1>
         </div>
     )
 }
-export default Splash
