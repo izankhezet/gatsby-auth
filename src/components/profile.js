9787d1230e6ffd241c54f8152a64be1fdc1769d2
siemah <m_hamiche@inptic.edu.dz>
2019-09-17 04:02:15 +0100

    fetching loggedin user details

--- src/components/profile.js
+++ src/components/profile.js
@@ -4,12 +4,27 @@ import { getUser } from '../services/auth';
 
 export default function ProfilePage() {
   const { user_display_name } = getUser();
+  const onFetchMoreDetails = async () => {
+    try {
+      let { id, token } = getUser();
+      let _res = await fetch(`https://api.aalladine.com/wp-json/wp/v2/user/${id}`, {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      })
+      console.log(await _res.json())
+    } catch (error) {
+      console.log(error) 
+      alert(error.message)     
+    }
+  }
   return (
     <>
       <SEO title="Dashboard - profile" />
       <h1>
         Welcome { user_display_name } to your dashboard
       </h1>
+      <button onClick={onFetchMoreDetails}>Get User Details</button>
     </>
   )
 }
