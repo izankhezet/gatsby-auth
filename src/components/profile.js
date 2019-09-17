011d2cf5df6ed3481b38b7b6011a6639761138aa
siemah <m_hamiche@inptic.edu.dz>
2019-09-17 03:16:55 +0100

    display name depend on wp jwt response

--- src/components/profile.js
+++ src/components/profile.js
@@ -3,12 +3,12 @@ import SEO from "./seo"
 import { getUser } from '../services/auth';
 
 export default function ProfilePage() {
-  const { name } = getUser();
+  const { user_display_name } = getUser();
   return (
     <>
       <SEO title="Dashboard - profile" />
       <h1>
-        Welcome { name } to your dashboard
+        Welcome { user_display_name } to your dashboard
       </h1>
     </>
   )
