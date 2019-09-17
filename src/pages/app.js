0a6a694cd6954e5c3d8a58f487b4e3d7efba975a
siemah <m_hamiche@inptic.edu.dz>
2019-09-17 01:33:07 +0100

    render WPLogin page to auth via wordpress jwt

--- src/pages/app.js
+++ src/pages/app.js
@@ -4,6 +4,7 @@ import { Router } from "@reach/router"
 import Layout from "../components/layout"
 import Profile from "../components/profile"
 import Login from "../components/Login"
+import WPLogin from '../components/WPLogin'
 import PrivateRoute from "../components/privateRoute";
 
 const App = () => (
@@ -11,6 +12,7 @@ const App = () => (
     <Router>
       <PrivateRoute component={Profile} path="/app/profile" />
       <Login path="/app/login" />
+      <WPLogin path='/app/wplogin' />
     </Router>
   </Layout>
 )
