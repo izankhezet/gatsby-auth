b27c12a6a3b3a7911c58ca24daa052eed73b7817
siemah <m_hamiche@inptic.edu.dz>
2019-09-17 03:12:36 +0100

    Check if user has been logged or not

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -1,47 +1,50 @@
 import React, { useState, useEffect, useContext } from 'react';
 
-import { checkingAuth, setUser, } from '../services/auth';
+import { checkingWPAuth, setUser, getUser, } from '../services/auth';
 import { AuthContext } from '../store/context/auth';
 
-const Splash = ({children}) =>  {
+const Splash = ({ children }) => {
   const [loaded, setLoaded] = useState(false);
   const { setAuth } = useContext(AuthContext);
 
   useEffect(() => {
-    const fetch = async () => {
+    const userHasValideToken = async () => {
       try {
-        let { data } = await checkingAuth();
-        if( data && data.status === 'OK' ) {
-            setUser(data.user);// save loggedin user details in cookies or localStorage
-            setAuth(data.user);// update the context of auth by the loggedin user details
-        } else alert('Network Failed :(');
-        setLoaded(true);
+        let _isValideToken = await checkingWPAuth();
+        console.log(_isValideToken)
+        if(_isValideToken) 
+          setAuth({
+            username: getUser().user_nicename,
+            email: getUser().user_email
+          })
+        else setUser({});
       } catch (e) {
         setUser({});
-        alert("fired an error ", e.message);
+        console.log("fired an error =>", e);
+      } finally {
         setLoaded(true);
       }
     }
-    fetch()
+    userHasValideToken()
   }, []);
 
-  if(loaded) return <>{ children }</>
+  if (loaded) return <>{children}</>
   return (
-        <div style={{
-            position:'fixed',
-            top: 0,
-            left: 0,
-            height: '100vh',
-            width: '100vw',
-            backgroundColor: '#333',
-            zIndex: 1000,
-            color: '#666',
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center'
-        }}>
-            <h1>Splash Screen</h1>
-        </div>
-    )
+    <div style={{
+      position: 'fixed',
+      top: 0,
+      left: 0,
+      height: '100vh',
+      width: '100vw',
+      backgroundColor: '#333',
+      zIndex: 1000,
+      color: '#666',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center'
+    }}>
+      <h1>Splash Screen</h1>
+    </div>
+  )
 }
 export default Splash;
